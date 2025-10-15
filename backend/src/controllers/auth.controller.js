90b0b3caa3d930f9ff659de8dd5f650de435637e
G K M Perera <158121610+PereraMadhushani@users.noreply.github.com>
2025-10-15 23:07:09 +0530

    welcome email implemented.

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
-import { generateToken } from "../lib/util.js"
-
+import { generateToken } from "../lib/util.js";
+import { sendWelcomeEmail } from "../emails/emaillHandlers.js";
+import { ENV } from "../lib/env.js";
 
 export const signup =async (req, res) =>{
     const {fullName, email,password} = req.body
@@ -49,6 +50,12 @@ export const signup =async (req, res) =>{
                 email:newUser.email,
                 profilePic:newUser.profilePic,
             });
+
+            try{
+                await sendWelcomeEmail(saveUser.email, saveUser.fullName,ENV.CLIENT_URL);
+            } catch (error){
+                console.log("Error sending welcome email:", error);
+            }
         } else{
             res.status(400).json({message:"Invalid user data"})
         }
